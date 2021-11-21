4b273275aa553e11af23440160d9f79a6423cd93
VivekPandey2428 <vivekpandey518392@gmail.com>
2021-11-21 16:34:46 +0530

    hello

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'relevel';
+  data=[
+    {id:Math.random(),heading:"Ticket1",desc:"Please add a Card here"},
+    {id:Math.random(),heading:"Ticket2",desc:"Card already added"},
+    {id:Math.random(),heading:"Ticket3",desc:"Card already added"},
+  ]
 }
